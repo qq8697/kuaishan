a854545d4efe3b078b5ff97977440466b4c15686
qq8697_whb <1058806283@qq.com>
2017-10-29 17:03:27 +0800

    add js file

---
+++ config.js
@@ -0,0 +1,30 @@
+if(location.host.indexOf('mgd5.com') != -1){
+	window._mrmcp = window._mrmcp || {}
+	_mrmcp.ticketUrl="http://card.mgd5.com/weixin/card/tickets/mgd5_wx_js_ticket.json";
+	_mrmcp.weixinAppId = 'wx49b70638f601d959';
+}
+else if(location.host.indexOf('h5mc.com') != -1){
+	window._mrmcp = window._mrmcp || {}
+	_mrmcp.ticketUrl="http://card.mgd5.com/weixin/card/tickets/mgd5_wx_js_ticket.json";
+	_mrmcp.weixinAppId = 'wx49b70638f601d959';
+}
+else if(location.host.indexOf('imugeda.com') != -1){
+	window._mrmcp = window._mrmcp || {}
+	_mrmcp.ticketUrl="http://card.mugeda.com/weixin/card/tickets/case_wx_js_ticket.json";
+	_mrmcp.weixinAppId = 'wx5b0e5c28d5a6b1ad';
+}
+else if(location.host.indexOf('mugeda.com') != -1){
+	window._mrmcp = window._mrmcp || {}
+	_mrmcp.ticketUrl="http://card.mgd5.com/weixin/card/tickets/mgd5_wx_js_ticket.json";
+	_mrmcp.weixinAppId = 'wx49b70638f601d959';
+}
+else if(location.host.indexOf('h5mgd.com') != -1){
+	window._mrmcp = window._mrmcp || {}
+	_mrmcp.ticketUrl="http://card.mugeda.com/weixin/card/tickets/case_wx_js_ticket.json";
+	_mrmcp.weixinAppId = 'wx5b0e5c28d5a6b1ad';
+}
+else if(location.host.indexOf('mgdh5.com') != -1){
+	window._mrmcp = window._mrmcp || {}
+	_mrmcp.ticketUrl="http://card.mugeda.com/weixin/card/tickets/da_wx_js_ticket.json";
+	_mrmcp.weixinAppId = 'wx0da814b613991d9f';
+}
